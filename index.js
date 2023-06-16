24ad38d10cb9f00550c551e2af41c43519488123
mary446 <135317707+mary446@users.noreply.github.com>
2023-06-16 21:06:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin (){
+const superbowlWin = '2015';
+return superbowlWin;
+}
+function sadReality() {
+const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+    return sadReality;
+}
+
